9241532dae1cee5d7ac8d4ef1da5349a3adc5dea
Needlide <max.litvinyuck553@gmail.com>
2025-04-22 21:30:19 +0300

    feat: implemented auth and role guards to protect endpoints from unauthorized access

---
+++ src/app/core/role.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
+
+export const roleGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const expectedRoles = route.data['roles'] as string[];
+  const userRole = authService.getUserRole();
+
+  if (!userRole || !expectedRoles.includes(userRole)) {
+    router.navigate(['/access-denied']);
+    return false;
+  }
+
+  return true;
+};
