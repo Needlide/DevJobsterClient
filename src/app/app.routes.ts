3d5ac3f9009518cfe4b3ca82729f85f0d048cdec
Needlide <max.litvinyuck553@gmail.com>
2025-04-22 21:27:53 +0300

    feat: configured routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/login/login.component';
+import { AccessDeniedComponent } from './core/access-denied/access-denied.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { RegisterComponent } from './core/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // {
+  //   path: 'dashboard',
+  //   component: DashboardComponent,
+  //   canActivate: [AuthGuard],
+  // },
+  // {
+  //   path: 'recruiter',
+  //   component: RecruiterComponent,
+  //   canActivate: [AuthGuard, RoleGuard],
+  //   data: { roles: ['recruiter'] },
+  // },
+  // {
+  //   path: 'admin',
+  //   component: AdminPanelComponent,
+  //   canActivate: [AuthGuard, RoleGuard],
+  //   data: { roles: ['admin'] },
+  // },
+  // {
+  //   path: 'user',
+  //   component: UserDashboardComponent,
+  //   canActivate: [AuthGuard, RoleGuard],
+  //   data: { roles: ['user'] },
+  // },
+  {
+    path: 'access-denied',
+    component: AccessDeniedComponent,
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
