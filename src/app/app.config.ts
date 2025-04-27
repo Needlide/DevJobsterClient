c726e460dad33dcc192dcf2b17023e0805ec596b
Needlide <max.litvinyuck553@gmail.com>
2025-04-27 23:28:16 +0300

    feat: enable animation provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/token.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideAnimationsAsync(),
   ],
 };
