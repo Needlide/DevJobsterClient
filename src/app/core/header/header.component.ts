f042b3d49c0d10b183afdad3bd386f7cf7211488
Needlide <max.litvinyuck553@gmail.com>
2025-04-27 23:29:30 +0300

    fix: import button module, restyle to match new look, when button became styled by Angular material

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, MatIconModule],
+  imports: [RouterModule, MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
