56fb2a1bba6c77b4c117ec7bbaba911d746074a7
Needlide <max.litvinyuck553@gmail.com>
2025-04-28 22:54:22 +0300

    feat: implement not-found page

---
+++ src/app/core/not-found/not-found.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [MatIconModule, MatButtonModule],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+})
+export class NotFoundComponent {
+  constructor(private router: Router) {}
+
+  goHome(): void {
+    this.router.navigate(['/']);
+  }
+}
