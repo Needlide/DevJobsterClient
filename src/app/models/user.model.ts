8a331c0bf4a28f6ea19d8678ad6b1568e9f01bbb
Needlide <max.litvinyuck553@gmail.com>
2025-05-02 20:22:49 +0300

    feat: add user account model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+  firstName: string;
+  lastName: string;
+  role: string;
+  skills?: string;
+  location: string;
+  yearsOfExperience: string;
+  englishLevel: string;
+}
