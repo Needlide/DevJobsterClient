7261694ec690e381ba43bcfc010494cca1ec4c7a
Needlide <max.litvinyuck553@gmail.com>
2025-05-04 17:08:48 +0300

    feat: implement dashboard container component for conditional rendering of different dashboards depending on user's role

---
+++ src/app/core/dashboard/dashboard.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { AdminDashboardComponent } from '../admin-dashboard/admin-dashboard.component';
+import { RecruiterDashboardComponent } from '../recruiter-dashboard/recruiter-dashboard.component';
+import { UserDashboardComponent } from '../user-dashboard/user-dashboard.component';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [
+    AdminDashboardComponent,
+    RecruiterDashboardComponent,
+    UserDashboardComponent,
+  ],
+  template: `
+    <app-admin-dashboard *ngIf="userRole === 'admin'"></app-admin-dashboard>
+    <app-recruiter-dashboard
+      *ngIf="userRole === 'recruiter'"
+    ></app-recruiter-dashboard>
+    <app-user-dashboard *ngIf="userRole === 'user'"></app-user-dashboard>
+  `,
+})
+export class DashboardComponent implements OnInit {
+  userRole: string | null = '';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userRole = this.authService.getUserRole();
+  }
+}
