2e7ce6ac84eba0b2c9a561c1fd1b9c6b9ffb1ce7
Needlide <max.litvinyuck553@gmail.com>
2025-05-09 22:31:16 +0300

    feat: add user update model

---
+++ src/app/models/user/user-update.model.ts
@@ -0,0 +1,9 @@
+export interface UserUpdate {
+  firstName?: string;
+  lastName?: string;
+  role?: string;
+  skills?: string;
+  location?: string;
+  yearsOfExperience?: number;
+  englishLevel?: string;
+}
