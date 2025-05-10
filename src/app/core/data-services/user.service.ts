054e1b2cebf01e39b9446012146906137bc3a369
Needlide <max.litvinyuck553@gmail.com>
2025-05-10 13:04:27 +0300

    feat: implement user API calling service

---
+++ src/app/core/data-services/user.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { UserProfileView } from '../../models/user/user-profile-view.model';
+import { Observable } from 'rxjs/internal/Observable';
+import { UserUpdate } from '../../models/user/user-update.model';
+import { LoginRegisterModel } from '../../models/login-register.model';
+import { UserAuthentication } from '../../models/user-authentication.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private apiUrl = `${environment.apiUrl}/users`;
+
+  constructor(private httpClient: HttpClient) {}
+
+  getAllUsers(): Observable<UserProfileView[]> {
+    return this.httpClient.get<UserProfileView[]>(`${this.apiUrl}/`);
+  }
+
+  getCurrentUser(): Observable<UserProfileView> {
+    return this.httpClient.get<UserProfileView>(`${this.apiUrl}/me`);
+  }
+
+  registerUser(model: LoginRegisterModel): Observable<any> {
+    return this.httpClient.post(`${this.apiUrl}/`, model);
+  }
+
+  updateUser(user: UserUpdate): Observable<any> {
+    return this.httpClient.put(`${this.apiUrl}/me`, user);
+  }
+
+  deleteUser(): Observable<any> {
+    return this.httpClient.delete(`${this.apiUrl}/me`);
+  }
+
+  resetPassword(userAuth: UserAuthentication): Observable<any> {
+    return this.httpClient.post(`${this.apiUrl}/reset-password`, userAuth);
+  }
+
+  getUserApplications(): Observable<UserProfileView[]> {
+    return this.httpClient.get<UserProfileView[]>(
+      `${this.apiUrl}/my-applications`
+    );
+  }
+
+  getUserChats(): Observable<any> {
+    return this.httpClient.get(`${this.apiUrl}/my-chats`);
+  }
+}
