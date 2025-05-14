a24e79281f3a54125dd8952ad246e922d01c838e
Needlide <max.litvinyuck553@gmail.com>
2025-05-14 22:46:38 +0300

    feat: properly configure user's profile update

--- src/app/core/user-profile/user-profile.component.ts
+++ src/app/core/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,6 +10,9 @@ import { MatRadioModule } from '@angular/material/radio';
 import { CommaSeparatedValidatorDirective } from './CommaSeparatedValidator/comma-separated-validator.directive';
 import { ExperienceValidatorDirective } from './ExperienceValidator/experience-validator.directive';
 import { HeaderComponent } from '../header/header.component';
+import { UserProfileView } from '../../models/user/user-profile-view.model';
+import { UserService } from '../data-services/user.service';
+import { UserUpdate } from '../../models/user/user-update.model';
 
 @Component({
   selector: 'app-user-profile',
@@ -29,7 +32,16 @@ import { HeaderComponent } from '../header/header.component';
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss',
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  user: UserProfileView | undefined;
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe({
+      next: (data) => (this.user = data),
+    });
+  }
+
   readonly countries: string[] = [
     'Afghanistan',
     'Albania',
@@ -40,7 +52,7 @@ export class UserProfileComponent {
     'United Kingdom',
     'Canada',
     'Australia',
-    // Add more countries as needed
+    'Ukraine',
   ];
 
   readonly englishLevels: string[] = [
@@ -52,13 +64,27 @@ export class UserProfileComponent {
     'Proficient (C2)',
   ];
 
-  role = '';
-  skills = '';
-  yearsOfExperience = '';
-  location = '';
-  englishLevel = '';
-
   buttonText = 'Edit';
 
-  onClick(): void {}
+  onClick(): void {
+    if (!this.user) {
+      return;
+    }
+
+    let updateUser: UserUpdate = {
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      role: this.user.role,
+      skills: this.user.skills,
+      location: this.user.location,
+      yearsOfExperience: this.user.yearsOfExperience,
+      englishLevel: this.user.englishLevel,
+    };
+
+    this.userService.updateUser(updateUser).subscribe({
+      next: () => {
+        this.buttonText = 'Saved';
+      },
+    });
+  }
 }
