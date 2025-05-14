829e51101edf53b41b9fa9292900d1c5c7c2588a
Needlide <max.litvinyuck553@gmail.com>
2025-05-14 16:25:27 +0300

    feat: implement modal dialog for updating the vacancy

---
+++ src/app/core/update-vacancy-dialog/update-vacancy-dialog.component.ts
@@ -0,0 +1,49 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { VacancyView } from '../../models/vacancy/vacancy.model';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-update-vacancy-dialog',
+  imports: [MatFormFieldModule, ReactiveFormsModule],
+  templateUrl: './update-vacancy-dialog.component.html',
+  styleUrl: './update-vacancy-dialog.component.scss',
+})
+export class UpdateVacancyDialogComponent {
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<UpdateVacancyDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: VacancyView
+  ) {
+    this.form = this.fb.group({
+      description: [data.description, Validators.required],
+      requirements: [data.requirements, Validators.required],
+      companyWebsite: [data.companyWebsite, Validators.required],
+      typeOfJob: [data.typeOfJob, Validators.required],
+      location: [data.location, Validators.required],
+      country: [data.country, Validators.required],
+    });
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onSave(): void {
+    if (this.form.valid) {
+      const updatedVacancy: VacancyView = {
+        ...this.data,
+        ...this.form.value,
+      };
+      this.dialogRef.close(updatedVacancy);
+    }
+  }
+}
