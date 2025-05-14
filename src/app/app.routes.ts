bc87a91ca7038cef681dd0bb2145c15480a1d571
Needlide <max.litvinyuck553@gmail.com>
2025-05-14 22:53:12 +0300

    feat: complete the routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,37 +3,67 @@ import { LoginComponent } from './core/login/login.component';
 import { AccessDeniedComponent } from './core/access-denied/access-denied.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { RegisterComponent } from './core/register/register.component';
-
+import { UserProfileComponent } from './core/user-profile/user-profile.component';
+import { authGuard } from './core/auth.guard';
+import { roleGuard } from './core/role.guard';
+import { AccountReviewPageComponent } from './core/account-review-page/account-review-page.component';
+import { ReportReviewComponent } from './core/report-review/report-review.component';
+import { AppliedJobsComponent } from './core/applied-jobs/applied-jobs.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { RecruiterProfileComponent } from './core/recruiter-profile/recruiter-profile.component';
+import { ApplicantsComponent } from './core/applicants/applicants.component';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  // {
-  //   path: 'dashboard',
-  //   component: DashboardComponent,
-  //   canActivate: [AuthGuard],
-  // },
-  // {
-  //   path: 'recruiter',
-  //   component: RecruiterComponent,
-  //   canActivate: [AuthGuard, RoleGuard],
-  //   data: { roles: ['recruiter'] },
-  // },
-  // {
-  //   path: 'admin',
-  //   component: AdminPanelComponent,
-  //   canActivate: [AuthGuard, RoleGuard],
-  //   data: { roles: ['admin'] },
-  // },
-  // {
-  //   path: 'user',
-  //   component: UserDashboardComponent,
-  //   canActivate: [AuthGuard, RoleGuard],
-  //   data: { roles: ['user'] },
-  // },
+  {
+    path: 'user',
+    component: UserProfileComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['user'] },
+  },
+  {
+    path: 'recruiter',
+    component: RecruiterProfileComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['recruiter'] },
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'applied-jobs',
+    component: AppliedJobsComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['user'] },
+  },
+  {
+    path: 'account-details/:userId',
+    component: AccountReviewPageComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['admin'] },
+  },
+  {
+    path: 'report-detail',
+    component: ReportReviewComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['admin'] },
+  },
+  {
+    path: 'applicants',
+    component: ApplicantsComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['recruiter'] },
+  },
   {
     path: 'access-denied',
     component: AccessDeniedComponent,
   },
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [authGuard, roleGuard],
+  },
   { path: '**', component: NotFoundComponent },
 ];
