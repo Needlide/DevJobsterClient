62c9f366a254bcdbdb61bfc3c1622c5d4b385c11
Needlide <max.litvinyuck553@gmail.com>
2025-05-14 20:58:08 +0300

    feat: implement applicants component using vacancy service

--- src/app/core/applicants/applicants.component.ts
+++ src/app/core/applicants/applicants.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserApplicationView } from '../../models/user/user-application-view.model';
 import { CommonModule } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { ActivatedRoute } from '@angular/router';
+import { VacancyService } from '../data-services/vacancy.service';
 
 @Component({
   selector: 'app-applicants',
@@ -9,6 +11,22 @@ import { MatExpansionModule } from '@angular/material/expansion';
   templateUrl: './applicants.component.html',
   styleUrl: './applicants.component.scss',
 })
-export class ApplicantsComponent {
+export class ApplicantsComponent implements OnInit {
   applicants: UserApplicationView[] = [];
+  vacancyId: string | null = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private vacancyService: VacancyService
+  ) {}
+
+  ngOnInit(): void {
+    this.vacancyId = this.route.snapshot.paramMap.get('userId');
+
+    if (this.vacancyId) {
+      this.vacancyService.getApplicationsByVacancy(this.vacancyId).subscribe({
+        next: (data) => (this.applicants = data),
+      });
+    }
+  }
 }
