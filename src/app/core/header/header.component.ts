8f880f046498df7cd17b1c20b0832e36afd5b251
Needlide <max.litvinyuck553@gmail.com>
2025-05-25 20:54:33 +0300

    feat: implement conditional rendering of a link accessible only to user role

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +10,15 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  isUser: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    let role = this.authService.getUserRole();
+
+    if (role == 'user') this.isUser = true;
+    else this.isUser = false;
+  }
+}
