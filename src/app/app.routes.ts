622e4edaa3f8a039d7008e6b0747fc71067c2418
Needlide <max.litvinyuck553@gmail.com>
2025-05-25 20:57:00 +0300

    fix: add the path property for applicants route, remove unused imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,12 @@ import { LoginComponent } from './core/login/login.component';
 import { AccessDeniedComponent } from './core/access-denied/access-denied.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { RegisterComponent } from './core/register/register.component';
-import { UserProfileComponent } from './core/user-profile/user-profile.component';
 import { authGuard } from './core/auth.guard';
 import { roleGuard } from './core/role.guard';
 import { AccountReviewPageComponent } from './core/account-review-page/account-review-page.component';
 import { ReportReviewComponent } from './core/report-review/report-review.component';
 import { AppliedJobsComponent } from './core/applied-jobs/applied-jobs.component';
 import { DashboardComponent } from './core/dashboard/dashboard.component';
-import { RecruiterProfileComponent } from './core/recruiter-profile/recruiter-profile.component';
 import { ApplicantsComponent } from './core/applicants/applicants.component';
 import { ProfileComponent } from './core/profile/profile.component';
 export const routes: Routes = [
@@ -45,7 +43,7 @@ export const routes: Routes = [
     data: { roles: ['admin'] },
   },
   {
-    path: 'applicants',
+    path: 'applicants/:vacancyId',
     component: ApplicantsComponent,
     canActivate: [authGuard, roleGuard],
     data: { roles: ['recruiter'] },
